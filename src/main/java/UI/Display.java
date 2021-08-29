54e477aee247dba7cddb41ea449a73977b6955f0
DDubicki <dubickidymitr@gmail.com>
2021-08-29 17:58:23 +0300

    Display game menu. Colors added. Display boarding phase messages

--- src/main/java/UI/Display.java
+++ src/main/java/UI/Display.java
@@ -2,8 +2,27 @@ package UI;
 
 import Squares.Board;
 
-public class Display {
+public class Display extends Input{
     public void printGameMenu() {
+        System.out.println(Colors.PURPLE + " * * * ");
+        System.out.println("Tu-Tu-Tu-Tu. Welcome in Battleships!!!");
+        System.out.println(Colors.PURPLE + " * * * ");
+        System.out.println("Please provide a first player name: ");
+        setFirstPlayer(getPlayerName());
+        System.out.println("Please provide a second player name: ");
+        setSecondPlayer(getPlayerName());
+    }
+
+    public void boardingPhase(int turnCounter){
+        if (turnCounter % 2 == 0){
+            System.out.println(getFirstPlayer() + ", your turn to place ships");
+        } else {
+            System.out.println(getSecondPlayer() + ", your turn to place ships");
+        }
+    }
+
+    public void gamePhase(){
+
     }
 
     public void printBoard(Board board) {
--- src/main/java/UI/Input.java
+++ src/main/java/UI/Input.java
@@ -1,10 +1,30 @@
 package UI;
 
+import java.util.Scanner;
+
 public class Input {
+    private String firstPlayer;
+    private String secondPlayer;
     private Input input;
 
+    public String getFirstPlayer() {
+        return firstPlayer;
+    }
+    public String getSecondPlayer() {
+        return secondPlayer;
+    }
+
+    public void setFirstPlayer(String firstPlayer) {
+        this.firstPlayer = firstPlayer;
+    }
+    public void setSecondPlayer(String secondPlayer) {
+        this.secondPlayer = secondPlayer;
+    }
+
     public String getPlayerName() {
-        return null;
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine().trim();
+        return name;
     }
 
     public char getCoordinateX() {
